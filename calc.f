b2b8aa19c42cc6137269966120d6c6d007adc0e9
Youngung Jeong <yjeong@changwon.ac.kr>
2020-04-20 15:00:36 +0900

    Added a few comments

--- calc.f
+++ calc.f
@@ -1,13 +1,12 @@
 c     subroutine calc is to calculate the area of a circle.
       subroutine calc(r)
       real area, r
-	
-	
+      write(*,*)'Begin calc subroutine'
+
       area=r*r*3.141592
-	
+
       write(*,*)'area:',area
-	  
       write(*,*)'finished'
-	
+
       return
       end subroutine calc
